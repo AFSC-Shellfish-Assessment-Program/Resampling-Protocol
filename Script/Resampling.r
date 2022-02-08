c9965ae559df07f8734513a7c41bb866ec449020
LeahZacher <leah.zacher@noaa.gov>
2022-02-08 12:09:04 -0900

    changed color of leg 1 retow hist

--- Script/Resampling.r
+++ Script/Resampling.r
@@ -184,7 +184,7 @@ crab_ebs %>%
   mutate(haul_type = recode(haul_type, "3" = "Leg 1", "17" = "Resampling")) %>%
   group_by(year) %>%
   ggplot()+
-  geom_histogram(aes(x=cl), binwidth=2, color = "#000000", fill = "blue") +
+  geom_histogram(aes(x=cl), binwidth=2, color = "#000000", fill = "cyan") +
   facet_wrap(~haul_type) +
   theme_bw() +
   xlab("Carapace Length (mm)") +
